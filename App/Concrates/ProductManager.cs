3960c6d079ecca82595ff951fb72af7452799fa3
Enes Alıcı <enesaalici@gmail.com>
2024-04-24 16:15:43 +0300

    first commit

---
+++ App/Concrates/ProductManager.cs
@@ -0,0 +1,37 @@
+﻿using Business.Abstracts;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrates
+{
+    public class ProductManager : IProductService
+    {
+        List<Product> products;
+
+        public ProductManager()
+        {
+            this.products = new List<Product>();
+        }
+
+        public void Add(Product product)
+        {
+            products.Add(product);
+        }
+
+        public void Delete(int id)
+        {
+            Console.Write("silme işlemi tamamnaldı");
+        }
+
+        public List<Product> GetAll()
+        {
+            return this.products;
+        }
+    }
+}
---
+++ WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using Business.Abstracts;
+using Business.Concrates;
+using Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        IProductService productService;
+
+        public ProductsController(IProductService productService)
+        {
+            this.productService = productService;
+        }
+
+        [HttpGet]
+        public List<Product> GetAll()
+        {
+            return productService.GetAll();
+        }
+
+        [HttpPost]
+        public void Add(Product product)
+        {
+            productService.Add(product);
+        }
+
+        
+    }
+}
